4c047ae9ce50337f32226e11be10bd69140b38b8
ViolandAbukachi <violandabukachi@gmail.comroot@30216a731d37:/alx-pre_course# git config --global user.email violandabukachi@gmail.com>
2023-03-17 10:51:52 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchars('\n');
+	return (0);
+}
